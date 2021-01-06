62c0c5af21b4976585c0c4d957d6691a9b073cad
unknown <nituldeori63@gmail.com>
2021-01-07 00:55:28 +0530

    Refactored: Ability to save the employee payroll object to local storage

--- js/new_payroll_form.js
+++ js/new_payroll_form.js
@@ -75,9 +75,9 @@ function createAndUpdateStorage(employeePayrollData){
     let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
 
     if(employeePayrollList != undefined){
-        employeePayrollList.push(EmployeePayrollData);
+        employeePayrollList.push(employeePayrollData);
     } else{
-        employeePayrollList = [EmployeePayrollData]
+        employeePayrollList = [employeePayrollData]
     }
     alert(employeePayrollList.toString());
     localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList))
